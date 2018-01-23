6dc0f09c3feb50154bc0015a3101c7edbea44f1d
subham <subham.bhattacharjee1@gmail.com>
2018-01-23 15:12:15 +0530

    Newly added problems

---
+++ spacetest.py
@@ -0,0 +1,3 @@
+a               = 12
+lolol           = 10
+koaskdoasdkaosd = 123
---
+++ untitled.py
@@ -0,0 +1,11 @@
+import requests
+import sys
+
+key = sys.argv[1]
+print key
+# base = 'http://api.data.gov/USDA/ERS/data/Arms/Surveys'
+# url = '%s?api_key=%s' % (base, key)
+
+# response = requests.get(url)
+
+# print response.json()
